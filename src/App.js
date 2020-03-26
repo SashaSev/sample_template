4caf3b386bad6a46610e5a5258ebaf6bc32b2039
SashaSev <www.sashsvardanyan1995@gmail.com>
2020-03-26 15:02:14 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, Suspense} from 'react';
+import Page1 from "./components/Page1";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+// import Page2 from "./components/Page2";
+// import AsyncComponent from './components/asyncComponent';
+const Page2lazy = React.lazy(() => import('./components/Page2'));
+const Page3lazy = React.lazy(() => import('./components/Page3'));
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            route: 'page1'
+        }
+    }
+
+
+    onRouteChanges = (route) => {
+        this.setState({
+            route: route
+        })
+    };
+
+    render() {
+        const {route} = this.state;
+        if (route === 'page1') {
+            return <Page1 onRouteChanges={this.onRouteChanges}/>
+        } else if (route === 'page2') {
+            return (
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Page2lazy onRouteChanges={this.onRouteChanges}/>
+                </Suspense>
+            )
+        } else if (route === 'page3') {
+            return (
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Page3lazy onRouteChanges={this.onRouteChanges}/>
+                </Suspense>
+            )
+        }
+    }
 }
 
 export default App;
